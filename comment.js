d577b02634442340110ddaaae9b73526697120d2
Kazunori Kouyama <147023274+k-kouyama@users.noreply.github.com>
2024-05-15 06:43:51 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,81 @@
+// Create Web Server
+const express = require('express');
+const app = express();
+const path = require('path');
+const { v4: uuid } = require('uuid');
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Array of comments
+let comments = [
+    {
+        id: uuid(),
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        id: uuid(),
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        id: uuid(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        id: uuid(),
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+// Home Page
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
+
+// Add Comment Page
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+// Create Comment
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() });
+    res.redirect('/comments');
+})
+
+// Show Comment
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment });
+})
+
+// Edit Comment Page
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+})
+
+// Update Comment
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
+})
+
+// Delete Comment
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
